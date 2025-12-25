259d59f4a1c51a0359b1e6da293a27d466d430c5
Cobiscuit <cobainirwin@gmail.com>
2025-12-25 17:16:25 -0600

    Seperated the stats alg from the main.py and then imported the info back into main.py.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+from stats import count_words
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+    
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    word_count = count_words(book_text)
+    print(f"Found {word_count} total words")
+    
+
+main()
